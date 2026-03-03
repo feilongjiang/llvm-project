39fb0521f1d0eda81090edc7350ff2b0e900463e
Feilong Jiang <freedom.jfl@gmail.com>
2026-03-03 14:49:56 +0800

    [Cpu0] Add Cpu0DAGToDAGISel

---
+++ llvm/lib/Target/Cpu0/Cpu0SEISelDAGToDAG.h
@@ -0,0 +1,38 @@
+//===-- Cpu0SEISelDAGToDAG.h - A Dag to Dag Inst Selector for Cpu0SE -----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Subclass of Cpu0DAGToDAGISel specialized for Cpu032.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+#define LLVM_LIB_TARGET_CPU0_CPU0SEISELDAGTODAG_H
+
+#include "Cpu0ISelDAGToDAG.h"
+
+namespace llvm {
+
+class Cpu0SEDAGToDAGISel : public Cpu0DAGToDAGISel {
+
+public:
+  explicit Cpu0SEDAGToDAGISel(Cpu0TargetMachine &TM, CodeGenOpt::Level OL)
+      : Cpu0DAGToDAGISel(TM, OL) {}
+
+private:
+  bool runOnMachineFunction(MachineFunction &MF) override;
+
+  bool trySelect(SDNode *Node) override;
+
+  void processFunctionAfterISel(MachineFunction &MF) override;
+};
+
+FunctionPass *createCpu0SEISelDag(Cpu0TargetMachine &TM,
+                                  CodeGenOpt::Level OptLevel);
+} // namespace llvm
+
+#endif
